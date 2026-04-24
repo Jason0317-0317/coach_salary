c7cba0550ce734c7e5c838b082f35ca11ac4cb57
Jason0317-0317 <youchengzhang215@gmail.com>
2026-04-24 16:07:18 +0800

    Update 教練獎金計算.py

--- 教練獎金計算.py
+++ 教練獎金計算.py
@@ -37,44 +37,56 @@ def generate_perfect_salary_report(uploaded_files):
         "邱靜瑜": {"團1-2人": 600, "團3人": 650, "團4人": 700, "團5人": 750, "團6人": 800, "1對2(1.5hr)": 975, "1對2": 650, "1對1(1.5hr)": 975, "1對1": 650}
     }
 
-    NAME_CONVERSION = { "意潔": "林意潔", "Vivi": "陳秀蓉", "怡廷": "陳怡廷", "佳蓁": "鍾佳蓁 Rita", "宛婷": "黃宛婷", "小在": "楊子慧(小在)", "LOUIS": "許力尹 LOUIS", "顥顥": "顥顥", "睿絃": "洪睿絃", "儒蓁": "紀儒蓁", "翎瑋": "李翎瑋", "奕伶": "郭奕伶", "品均": "郭品均", "妍語": "邴妍語", "鈞弼": "張鈞弼", "竣升": "蕭竣升", "萃萃": "紀萃文", "函豫": "李函豫", "子綺": "尤子綺", "楷翌": "張楷翌", "懿庭": "侯懿庭", "俐池": "謝俐池", "姿菁": "黃姿菁", "郁雯": "籃郁雯", "徐漫": "徐漫", "筠馨": "鄭筠馨", "舒涵": "高舒涵", "靜瑜": "邱靜瑜" }
-    SENIORITY_DATA = { "林意潔": "兩年以上", "陳秀蓉": "兩年以上", "陳怡廷": "兩年以上", "鍾佳蓁 Rita": "兩年以上", "黃宛婷": "一年以上至兩年以下", "楊子慧(小在)": "一年以上至兩年以下", "許力尹 LOUIS": "一年以上至兩年以下", "顥顥": "一年以上至兩年以下", "洪睿絃": "一年以上至兩年以下", "紀儒蓁": "兩年以上", "李翎瑋": "一年以上至兩年以下", "郭奕伶": "一年以上至兩年以下", "郭品均": "一年以上至兩年以下", "邴妍語": "一年以上至兩年以下", "張鈞弼": "一年以上至兩年以下", "蕭竣升": "一年以上至兩年以下", "紀萃文": "一年以上至兩年以下", "李函豫": "一年以上至兩年以下", "尤子綺": "一年以下", "張楷翌": "一年以上至兩年以下", "侯懿庭": "一年以下", "謝俐池": "一年以下", "黃姿菁": "一年以下", "籃郁雯": "一年以下", "徐漫": "一年以下", "鄭筠馨": "一年以下", "高舒涵": "一年以下", "邱靜瑜": "一年以下" }
+    NAME_CONVERSION = {
+        "意潔": "林意潔", "Vivi": "陳秀蓉", "怡廷": "陳怡廷", "佳蓁": "鍾佳蓁 Rita", "宛婷": "黃宛婷",
+        "小在": "楊子慧(小在)", "LOUIS": "許力尹 LOUIS", "顥顥": "顥顥", "睿絃": "洪睿絃", "儒蓁": "紀儒蓁",
+        "翎瑋": "李翎瑋", "奕伶": "郭奕伶", "品均": "郭品均", "妍語": "邴妍語", "鈞弼": "張鈞弼",
+        "竣升": "蕭竣升", "萃萃": "紀萃文", "函豫": "李函豫", "子綺": "尤子綺", "楷翌": "張楷翌",
+        "懿庭": "侯懿庭", "俐池": "謝俐池", "姿菁": "黃姿菁", "郁雯": "籃郁雯", "徐漫": "徐漫",
+        "筠馨": "鄭筠馨", "舒涵": "高舒涵", "靜瑜": "邱靜瑜"
+    }
+    
+    SENIORITY_DATA = {
+        "林意潔": "兩年以上", "陳秀蓉": "兩年以上", "陳怡廷": "兩年以上", "鍾佳蓁 Rita": "兩年以上",
+        "黃宛婷": "一年以上至兩年以下", "楊子慧(小在)": "一年以上至兩年以下", "許力尹 LOUIS": "一年以上至兩年以下",
+        "顥顥": "一年以上至兩年以下", "洪睿絃": "一年以上至兩年以下", "紀儒蓁": "兩年以上",
+        "李翎瑋": "一年以上至兩年以下", "郭奕伶": "一年以上至兩年以下", "郭品均": "一年以上至兩年以下",
+        "邴妍語": "一年以上至兩年以下", "張鈞弼": "一年以上至兩年以下", "蕭竣升": "一年以上至兩年以下",
+        "紀萃文": "一年以上至兩年以下", "李函豫": "一年以上至兩年以下", "尤子綺": "一年以下",
+        "張楷翌": "一年以上至兩年以下", "侯懿庭": "一年以下", "謝俐池": "一年以下",
+        "黃姿菁": "一年以下", "籃郁雯": "一年以下", "徐漫": "一年以下",
+        "鄭筠馨": "一年以下", "高舒涵": "一年以下", "邱靜瑜": "一年以下"
+    }
     
     course_types = ["團1-2人", "團3人", "團4人", "團5人", "團6人", "1對2(1.5hr)", "1對2", "1對1(1.5hr)", "1對1"]
-    
     columns = [
-        "教練", "課程", "單價", 
-        "義昌館堂數", "義昌館金額", "高美館堂數", "高美館金額", "中山館堂數", "中山館金額", 
-        "巨蛋館總堂數", "總金額", 
-        "三館總堂數", "三館總金額", 
-        "應付金額", "堂數達標獎金(加項)", "總計", "執行業務(扣款)", "補充保費(扣款)", "應付薪資", "年資"
+        "教練", "課程", "單價", "義昌館堂數", "義昌館金額", "高美館堂數", "高美館金額", "中山館堂數", "中山館金額", "三館總堂數", "三館總金額", "應付金額", "堂數達標獎金(加項)", "總計", "執行業務(扣款)", "補充保費(扣款)", "應付薪資", "年資"
     ]
 
     all_rows = []
     for coach in COACH_PRICING.keys():
         for course in course_types:
             row = {col: 0 for col in columns}
-            row["教練"], row["課程"], row["單價"], row["年資"] = coach, course, COACH_PRICING[coach].get(course, 0), SENIORITY_DATA.get(coach, "")
+            row["教練"], row["課程"], row["單價"], row["年資"] = coach, course, COACH_PRICING[coach].get(course, 0), ""
             all_rows.append(row)
     df_master = pd.DataFrame(all_rows)
 
+    coach_seniority = {}
     for file in uploaded_files:
         df_info = pd.read_excel(file, sheet_name='統計總表', nrows=1, header=None)
         loc_name = str(df_info.iloc[0, 1]).strip()
         file.seek(0)
         df_stats = pd.read_excel(file, sheet_name='統計總表', skiprows=2).fillna(0)
         
-        if "巨蛋" in loc_name:
-            s_col, a_col = "巨蛋館總堂數", "總金額"
-        else:
-            s_col, a_col = f"{loc_name}堂數", f"{loc_name}金額"
-            
+        s_col, a_col = f"{loc_name}堂數", f"{loc_name}金額"
         for _, s_row in df_stats.iterrows():
             raw_name = str(s_row['姓名']).strip().split(' ')[0]
             mapped_name = NAME_CONVERSION.get(raw_name, raw_name)
+            if '年資' in s_row and s_row['年資'] != 0:
+                coach_seniority[mapped_name] = str(s_row['年資']).strip()
             mask = (df_master["教練"] == mapped_name)
             for course in course_types:
-                if course in s_row and s_col in df_master.columns:
+                if course in s_row:
                     val = s_row[course]
                     df_master.loc[mask & (df_master["課程"] == course), s_col] = val
                     df_master.loc[mask & (df_master["課程"] == course), a_col] = val * COACH_PRICING.get(mapped_name, {}).get(course, 0)
@@ -82,31 +94,20 @@ def generate_perfect_salary_report(uploaded_files):
     final_dfs = []
     for coach in COACH_PRICING.keys():
         c_df = df_master[df_master["教練"] == coach].copy()
-        
+        seniority_val = SENIORITY_DATA.get(coach, coach_seniority.get(coach, ""))
         c_df["三館總堂數"] = c_df[["義昌館堂數", "高美館堂數", "中山館堂數"]].sum(axis=1)
         c_df["三館總金額"] = c_df[["義昌館金額", "高美館金額", "中山館金額"]].sum(axis=1)
-        
-        base_pay = c_df["三館總金額"].sum() + c_df["總金額"].sum()
-        bonus = 0
-        total_sum = base_pay + bonus
+        base_pay = c_df["三館總金額"].sum()
+        total_sum = base_pay 
         tax_10 = int(total_sum * 0.1) if total_sum >= 20000 else 0
         health_211 = int(total_sum * 0.0211) if total_sum >= 20000 else 0
         final_pay = total_sum - tax_10 - health_211
-        
-        for col, val in zip(["應付金額", "堂數達標獎金(加項)", "總計", "執行業務(扣款)", "補充保費(扣款)", "應付薪資"], [base_pay, bonus, total_sum, tax_10, health_211, final_pay]):
+        c_df["年資"] = seniority_val
+        for col, val in zip(["應付金額", "總計", "執行業務(扣款)", "補充保費(扣款)", "應付薪資"], [base_pay, total_sum, tax_10, health_211, final_pay]):
             c_df[col] = val
         final_dfs.append(c_df)
-        
         sub_row = {col: 0 for col in columns}
-        sub_row["教練"], sub_row["課程"] = coach, "小計"
-        for loc_prefix in ["義昌館", "高美館", "中山館"]:
-            sub_row[f"{loc_prefix}堂數"] = c_df[f"{loc_prefix}堂數"].sum()
-            sub_row[f"{loc_prefix}金額"] = c_df[f"{loc_prefix}金額"].sum()
-        sub_row["巨蛋館總堂數"] = c_df["巨蛋館總堂數"].sum()
-        sub_row["總金額"] = c_df["總金額"].sum()
-        sub_row["三館總堂數"] = c_df["三館總堂數"].sum()
-        sub_row["三館總金額"] = c_df["三館總金額"].sum()
-        sub_row["應付金額"], sub_row["堂數達標獎金(加項)"], sub_row["總計"], sub_row["執行業務(扣款)"], sub_row["補充保費(扣款)"], sub_row["應付薪資"], sub_row["年資"] = base_pay, bonus, total_sum, tax_10, health_211, final_pay, SENIORITY_DATA.get(coach, "")
+        sub_row["教練"], sub_row["課程"], sub_row["應付金額"], sub_row["總計"], sub_row["應付薪資"], sub_row["年資"] = coach, "小計", base_pay, total_sum, final_pay, seniority_val
         final_dfs.append(pd.DataFrame([sub_row]))
 
     df_final = pd.concat(final_dfs, ignore_index=True)
@@ -117,16 +118,12 @@ def generate_perfect_salary_report(uploaded_files):
         border, center = Border(top=thin, left=thin, right=thin, bottom=thin), Alignment(horizontal='center', vertical='center')
         for cell in ws[1]:
             cell.fill, cell.font, cell.alignment, cell.border = PatternFill(start_color="D9D9D9", end_color="D9D9D9", fill_type="solid"), Font(bold=True), center, border
-            
-        pay_idx = df_final.columns.get_loc("應付金額") + 1
-        senior_idx = df_final.columns.get_loc("年資") + 1
-        start_r = 2
+        start_r, pay_idx, senior_idx = 2, df_final.columns.get_loc("應付金額") + 1, df_final.columns.get_loc("年資") + 1
         for r in range(2, ws.max_row + 1):
             coach_val, next_coach = ws.cell(row=r, column=1).value, ws.cell(row=r + 1, column=1).value if r < ws.max_row else None
-            is_subtotal = ws.cell(row=r, column=2).value == "小計"
             for c in range(1, ws.max_column + 1):
                 ws.cell(row=r, column=c).border, ws.cell(row=r, column=c).alignment = border, center
-                if is_subtotal: ws.cell(row=r, column=c).fill = PatternFill(start_color="F2F2F2", end_color="F2F2F2", fill_type="solid")
+                if ws.cell(row=r, column=2).value == "小計": ws.cell(row=r, column=c).fill = PatternFill(start_color="F2F2F2", end_color="F2F2F2", fill_type="solid")
             if next_coach != coach_val:
                 ws.merge_cells(start_row=start_r, start_column=1, end_row=r, end_column=1)
                 for c in range(pay_idx, senior_idx + 1):
@@ -136,8 +133,8 @@ def generate_perfect_salary_report(uploaded_files):
     return output.getvalue()
 
 st.title("教練薪資結算系統")
-uploaded_files = st.file_uploader("上傳統計表 (.xlsx)", type=["xlsx"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("上傳預約統計表 (.xlsx)", type=["xlsx"], accept_multiple_files=True)
 if uploaded_files:
-    if st.button("生成最終排版明細"):
+    if st.button("開始計算薪資並排版"):
         final_excel_data = generate_perfect_salary_report(uploaded_files)
-        st.download_button(label="📥 下載更新版薪資清單", data=final_excel_data, file_name="薪資明細_語法修正版.xlsx")
+        st.download_button(label="下載薪資明細表", data=final_excel_data, file_name="教練薪資明細_最終版.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
